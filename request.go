862cb2d0d7b60831aa038305974dbe64b2f25af7
Qiyi Huang <46252024+qiyihuang@users.noreply.github.com>
2021-03-16 15:27:44 +1100

    doc/add comments

--- request.go
+++ request.go
@@ -9,11 +9,18 @@ import (
 	"net/http"
 )
 
+// Request stores Discord webhook request information
 type Request struct {
 	Msg Message
 	URL string
 }
 
+// httpPoster sends http POST requests. e.g. http.Client
+type httpPoster interface {
+	Post(url string, contentType string, body io.Reader) (*http.Response, error)
+}
+
+// formatBody serialises Message.
 func formatBody(msg Message) io.Reader {
 	// Marshal would never fail since Discord webhook message does not
 	// contain types not supported by Marshal.
@@ -23,13 +30,13 @@ func formatBody(msg Message) io.Reader {
 }
 
 func respError(resp *http.Response) error {
-	// Log only when Discord API message (usually error) exists.
 	var respBody map[string]interface{}
 	err := json.NewDecoder(resp.Body).Decode(&respBody)
 	if err != nil {
 		return err
 	}
 
+	//Discord API error message is written in "message" field in response body.
 	if message, ok := respBody["message"]; ok {
 		errMsg := "Discord API error: " + fmt.Sprintf("%v", message)
 		return errors.New(errMsg)
@@ -38,10 +45,8 @@ func respError(resp *http.Response) error {
 	return nil
 }
 
-var post = http.Post
-
 // send sends the message to Discord via http.
-func (r Request) send() (resp *http.Response, err error) {
+func (r Request) send(p httpPoster) (resp *http.Response, err error) {
 	err = validateURL(r.URL)
 	if err != nil {
 		return
@@ -53,10 +58,11 @@ func (r Request) send() (resp *http.Response, err error) {
 	}
 
 	body := formatBody(r.Msg)
-	resp, err = post(r.URL, "application/json", body)
+	resp, err = p.Post(r.URL, "application/json", body)
 	if err != nil {
 		return
 	}
+	defer resp.Body.Close()
 
 	err = respError(resp)
 	return
