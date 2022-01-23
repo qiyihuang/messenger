10be0251d855b84d58539439d6bace5400bd1c54
Qiyi Huang <46252024+qiyihuang@users.noreply.github.com>
2022-01-23 07:44:26 +0000

    chore/move Embed to top of the file

--- message.go
+++ message.go
@@ -7,6 +7,21 @@ type Message struct {
 	Embeds   []Embed `json:"embeds,omitempty"`
 }
 
+// Embed represents an embed object in message object.
+type Embed struct {
+	Title       string    `json:"title,omitempty"`
+	Description string    `json:"description,omitempty"`
+	URL         string    `json:"url,omitempty"`
+	Timestamp   Timestamp `json:"timestamp,omitempty"`
+	Color       int       `json:"color,omitempty"`
+	Footer      Footer    `json:"footer,omitempty"`
+	Image       Image     `json:"image,omitempty"`
+	Thumbnail   Thumbnail `json:"thumbnail,omitempty"`
+	Video       Video     `json:"video,omitempty"`
+	Author      Author    `json:"author,omitempty"`
+	Fields      []Field   `json:"fields,omitempty"`
+}
+
 // Timestamp represents the timestamp string in an embed object.
 // Format timestamp using .UTC().Format("2006-01-02T15:04:05-0700"),
 // Discord will convert it to local time on display.
@@ -56,19 +71,4 @@ type Field struct {
 	Name   string `json:"name"`
 	Value  string `json:"value"`
 	Inline bool   `json:"inline,omitempty"`
-}
-
-// Embed represents an embed object in message object.
-type Embed struct {
-	Title       string    `json:"title,omitempty"`
-	Description string    `json:"description,omitempty"`
-	URL         string    `json:"url,omitempty"`
-	Timestamp   Timestamp `json:"timestamp,omitempty"`
-	Color       int       `json:"color,omitempty"`
-	Footer      Footer    `json:"footer,omitempty"`
-	Image       Image     `json:"image,omitempty"`
-	Thumbnail   Thumbnail `json:"thumbnail,omitempty"`
-	Video       Video     `json:"video,omitempty"`
-	Author      Author    `json:"author,omitempty"`
-	Fields      []Field   `json:"fields,omitempty"`
 }
